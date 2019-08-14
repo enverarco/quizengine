d5f8c54a638a4fe6b333293785fe9e490c6a0b41
enverarco <admin@admins-MacBook-Pro.local>
2019-08-14 13:06:21 +0100

    implemented express generator

--- index.js
+++ index.js
@@ -1,72 +1,13 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+var express = require('express');
+var body = require('body-parser');
+var app = express();
 
-const server = http.createServer((req, res) => {
+app.use(express.static(__dirname + '/public'));
 
-  // Build file path
-  let filePath = path.join(
-    __dirname,
-    "public",
-    req.url === "/" ? "index.html" : req.url
-  );
-
-  // Extension of file
-  let extname = path.extname(filePath);
-
-  // Initial content type
-  let contentType = "text/html";
-
-  // Check ext and set content type
-  switch (extname) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-  }
-
-  // Check if contentType is text/html but no .html file extension
-  if (contentType == "text/html" && extname == "") filePath += ".html";
-
-  // log the filePath
-  console.log(filePath);
-
-  // Read File
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code == "ENOENT") {
-        // Page not found
-        fs.readFile(
-          path.join(__dirname, "public", "404.html"),
-          (err, content) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, "utf8");
-          }
-        );
-      } else {
-        //  Some server error
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      // Success
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
-  });
+app.get('/', function(req, res){
+  res.render('index.html');
 });
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5656, () => {
+    console.log('http://localhost:5656')
+})
---
+++ public/javascripts/script.js
@@ -0,0 +1,24 @@
+/*function ButtonAction(sectionid){
+  if(sectionid == '1'){
+    HideShow("section1", "traits");
+  }
+  else if(sectionid == '2'){
+    HideShow("traits", "career");
+  }
+  else {
+    HideShow("career", "results");
+    CalculateResults();
+  }
+}
+
+function HideShow(sectiontoHide,sectiontoReveal){
+  document.getElementById(sectiontoHide).style.display = "none";
+  document.getElementById(sectiontoReveal).style.display = "block";
+}*/
+
+/*
+var inputs = document.getElementsByClassName("preference"),
+x=inputs.length;
+while(x--)
+inputs[x].addEventListener("click", function(){ ButtonAppear(button1); });
+*/
